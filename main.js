c16d78e654109e9a9051af554f3afde8c15a9dc9
mona-n67 <mona.nasrolahi@yahoo.com>
2019-07-06 23:21:05 +0430

    tamrin1.product

---
+++ main.js
@@ -0,0 +1,6 @@
+var pro = ["refrigerator", "frezzer", "oven", "vacumecleaner", "washingclothes", "phone", "camera",  "juicer" ];
+var input=prompt('what product do you want?') ;
+console.log(input);
+var c = pro.indexOf(input);
+console.log(c);
+//end
